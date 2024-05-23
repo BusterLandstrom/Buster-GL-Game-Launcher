d678df14812b512d48e28293297a8621f1a80d28
BusterLandstrom <manenargjordavgurka@gmail.com>
2024-05-23 05:03:00 +0200

    Added base functionality for program manager and themes

---
+++ Items/ProgramManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher.Items
+{
+    internal class ProgramManager : INotifyPropertyChanged
+    {
+        private event PropertyChangedEventHandler _propertyChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { _propertyChanged += value; }
+            remove { _propertyChanged -= value; }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            _propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        // Start of variables
+
+        private Theme _currentTheme;
+        public Theme CurrentTheme 
+        {
+            get {  return _currentTheme; }
+            set 
+            { 
+                _currentTheme = value;
+                OnPropertyChanged(nameof(CurrentTheme));
+            }
+        }
+    }
+}
---
+++ Items/Theme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher.Items
+{
+    internal class Theme
+    {
+        public required string BackgroundColor {  get; set; }
+        public required string ForegroundColor { get; set; }
+        public required string SelectablePrimaryColor { get; set; }
+        public required string SelectableSecondaryColor { get; set; }
+        public required string SelectableLinkColor { get; set; }
+
+    }
+}
